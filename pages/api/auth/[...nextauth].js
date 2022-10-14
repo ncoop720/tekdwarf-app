56bdae17fb61c29f7eb22de20d2ec73bc577dbdd
unknown <ncoop720@yahoo.com>
2022-10-14 19:30:03 -0400

    downgrade next; remove mongodb

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,9 @@
 import NextAuth from 'next-auth'
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 import GoogleProvider from 'next-auth/providers/google'
-import clientPromise from '../../../lib/mongodb'
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export default NextAuth({
-  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
